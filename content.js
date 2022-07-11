640613fe0e7a6dd5a3dc31a27968ceeb38035f85
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2022-07-11 11:27:31 +0530

    final commit

--- content.js
+++ content.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+var amount_node_list2 = document.getElementsByClassName('_3Hghg');
+var amount_regex2 = /\d+.\d*/g;
+var counter = 0;
+var total_amount2 = 0
+for (i = 0; i < amount_node_list2.length; i++) 
+{
+    counter++;
+    current_amount = amount_node_list2[i].innerHTML;
+    if (current_amount.match(amount_regex2)) 
+    {
+        integer_amount = parseInt(current_amount.match(amount_regex2)[0])
+        total_amount2 += integer_amount;
+    }
+}
+console.log("Total spent = ₹ ", total_amount2);
+console.log("Total orders = ", counter);
--- swiggy.js
+++ swiggy.js
@@ -1,7 +1,29 @@
-
-
-let p = document.getElementsByTagName('h1');
-for(i=0;i<p.length;i++)
+let amount_node_list = document.getElementsByClassName('_3Hghg');
+let amount_regex = /\d+.\d*/g;
+var total_amount = 0
+for (i = 0; i < amount_node_list.length; i++) 
 {
-    p[i].innerHTML="Manav";
+    current_amount = amount_node_list[i].innerHTML;
+    if (current_amount.match(amount_regex)) 
+    {
+        integer_amount = parseInt(current_amount.match(amount_regex)[0])
+        total_amount += integer_amount;
+    }
 }
+document.getElementsByClassName("_3RkS-")[0].innerHTML = "Total amount spent on Swiggy so far is ₹ "+total_amount;
+document.getElementsByClassName("-dIwe")[0].innerHTML = "Total amount spent on Swiggy so far is ₹ "+total_amount;
+// console.log("Total amount spent on Swiggy so far is ₹", total_amount);
+
+
+
+
+// let p = document.getElementsByTagName('h1');
+// let q = document.getElementsByTagName('p');
+// for(i=0;i<p.length;i++)
+// {
+//     p[i].innerHTML="Manav";
+// }
+// for(i=0;i<q.length;i++)
+// {
+//     q[i].innerHTML="hello world";
+// }
